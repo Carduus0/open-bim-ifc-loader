f764b64d2fcd2e7682314cba8c043d95cb67749a
Carduus0 <s.carduus@gmail.com>
2024-06-11 23:35:08 +0300

    with a template .ifc file at the start

--- app.ts
+++ app.ts
@@ -2,10 +2,11 @@
 import * as THREE from "three";
 import * as WEBIFC from "web-ifc";
 import * as BUI from "@thatopen/ui";
-import Stats from "stats.js";
 import * as OBC from "@thatopen/components";
 
 const container = document.getElementById("container")!;
+const fileNameDisplay = document.getElementById("file-name");
+const buttonsContainer = document.getElementById("buttons-container");
 
 const components = new OBC.Components();
 
@@ -32,52 +33,19 @@ grids.create(world);
 
 world.scene.three.background = null;
 
-
-/* MD
-  ### 🚗🏎️ Getting IFC and fragments
-  ---
-  When we read an IFC file, we convert it to a geometry called Fragments. Fragments are a lightweight representation of geometry built on top of THREE.js `InstancedMesh` to make it easy to work with BIM data efficiently. All the BIM geometry you see in our libraries are Fragments, and they are great: they are lightweight, they are fast and we have tons of tools to work with them. But fragments are not used outside our libraries. So how can we convert an IFC file to fragments? Let's check out how:
-  */
-
 const fragments = components.get(OBC.FragmentsManager);
 const fragmentIfcLoader = components.get(OBC.IfcLoader);
-
-/* MD
-  :::info Why not just IFC?
-
-  IFC is nice because it lets us exchange data with many tools in the AECO industry. But your graphics card doesn't understand IFC. It only understands one thing: triangles. So we must convert IFC to triangles. There are many ways to do it, some more efficient than others. And that's exactly what Fragments are: a very efficient way to display the triangles coming from IFC files. 
-  :::
-  Once Fragments have been generated, you can export them and then load them back directly, without needing the original IFC file. Why would you do that? Well, because fragments can load +10 times faster than IFC. And the reason is very simple.   When reading an IFC, we must parse the file, read the implicit geometry, convert it to triangles (Fragments) and send it to the GPU. When reading fragments, we just take the triangles and send them, so it's super fast. 
-  :::danger How to use Fragments?
-  If you want to find out more about Fragments, check out the Fragments Manager tutorial.
-  :::
-  ### 🔭🔧 Calibrating the converter
-  ---
-  Now, we need to configure the path of the WASM files. What's WASM? It's a technology that lets us run C++ on the browser, which means that we can load IFCs super fast! These files are the compilation of our `web-ifc` library. You can find them in the github repo and in NPM. These files need to be available to our app, so you have 2 options:
-  - Download them and serve them statically.
-  - Get them from a remote server.
-
-  The easiest way is getting them from unpkg, and the cool thing is that you don't need to do it manually! It can be done directly by the tool just by writing the following:
-  */
 
 // await fragmentIfcLoader.setup();
 
 async function setupLoader() {
-  await fragmentIfcLoader.setup();
+  await fragmentIfcLoader.setup({  wasm: {
+    path: "./web-ifc.wasm",
+    absolute: true
+  }});
 }
 
 setupLoader();
-
-// If you want to the path to unpkg manually, then you can skip the line
-// above and set them manually as below:
-// fragmentIfcLoader.settings.wasm = {
-//     path: "https://unpkg.com/web-ifc@0.0.53/",
-//     absolute: true
-// }
-
-/* MD
-  Awesome! Optionally, we can exclude categories that we don't want to convert to fragments like very easily:
-*/
 
 const excludedCats = [
   WEBIFC.IFCTENDONANCHOR,
@@ -89,18 +57,9 @@ for (const cat of excludedCats) {
   fragmentIfcLoader.settings.excludedCategories.add(cat);
 }
 
-/* MD
-  We can further configure the conversion using the `webIfc` object. In this example, we will make the IFC model go to the origin of the scene (don't worry, this supports model federation):
-  */
-
 fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
 
 /* MD
-  ### 🚗🔥 Loading the IFC
-  ---
-  Next, let's define a function to load the IFC programmatically. We have hardcoded the path to one of our IFC files, but feel free to do this with any of your own files!
-
- :::info Opening local IFCs
 
   Keep in mind that the browser can't access the file of your computer directly, so you will need to use the Open File API to open local files.
 
@@ -108,17 +67,45 @@ fragmentIfcLoader.settings.webIfc.COORDINATE_TO_ORIGIN = true;
 */
 
 // async function loadIfc() {
-//   const file = await fetch("https://thatopen.github.io/engine_components/resources/small.ifc");
+//   const file = await fetch("./models/EJES-rEVIT.ifc");
 //   const data = await file.arrayBuffer();
 //   const buffer = new Uint8Array(data);
 //   const model = await fragmentIfcLoader.load(buffer);
 //   // model.name = "example";
 //   world.scene.three.add(model);
 // }
-//-----
-// Получаем элемент input
-const input = document.getElementById('ifcInput');
+let uuid = "";
+async function loadIfc() {
+  const file = await fetch("./models/EJES-rEVIT.ifc");
+  const data = await file.arrayBuffer();
+  const buffer = new Uint8Array(data);
+  const fragmentsGroup = await fragmentIfcLoader.load(buffer);
+  
+  // Вычисляем габаритный объем модели
+  const boundingBox = new THREE.Box3().setFromObject(fragmentsGroup);
+  const modelMinY = boundingBox.min.y;
 
+  // Перемещаем все объекты внутри items
+  fragmentsGroup.items.forEach(fragment => {
+    const mesh = fragment.mesh as THREE.Object3D;
+    if (mesh instanceof THREE.InstancedMesh) {
+      const matrix = new THREE.Matrix4();
+      for (let i = 0; i < mesh.count; i++) {
+        mesh.getMatrixAt(i, matrix);
+        matrix.setPosition(matrix.elements[12], matrix.elements[13] - modelMinY, matrix.elements[14]);
+        mesh.setMatrixAt(i, matrix);
+      }
+      mesh.instanceMatrix.needsUpdate = true;
+    } else if (mesh instanceof THREE.Mesh) {
+      (mesh as THREE.Mesh).position.y -= modelMinY; // Перемещаем каждый фрагмент вниз
+    }
+  });
+
+  world.scene.three.add(fragmentsGroup);
+  uuid = fragmentsGroup.uuid; // Сохраняем UUID загруженной группы
+}
+//-----
+const input = document.getElementById('ifcInput');
 if (input) {
   input.addEventListener('change', async (event) => {
     const inputElement = event.target as HTMLInputElement;
@@ -155,9 +142,14 @@ if (input) {
             (mesh as THREE.Mesh).position.y -= modelMinY; // Перемещаем каждый фрагмент вниз
           }
         });
-        // Добавляем загруженную модель в сцену
+
         world.scene.three.add(fragmentsGroup);
       }
+
+      if (fileNameDisplay) {
+        fileNameDisplay.textContent = `Loaded file: ${file.name}`;
+      }
+     
     }
   }, false);
 } else {
@@ -167,7 +159,7 @@ if (input) {
 /* MD
   ### 🎁 Exporting the result to fragments
   ---
-  Once you have your precious fragments, you might want to save them so that you don't need to open this IFC file each time your user gets into your app. Instead, the next time you can load the fragments directly. Defining a function to export fragments is as easy as this:
+  Once you have your precious fragments, you might want to save them so that you don't need to open this IFC file each time your user gets into your app. Instead, the next time you can load the fragments directly. 
 */
 
 function download(file: File) {
@@ -193,6 +185,23 @@ async function exportFragments() {
   }
 }
 
+//----
+// async function exportAllFragments() {
+//   if (!fragments.groups.size) {
+//     return;
+//   }
+
+//   fragments.groups.forEach((group, groupId) => {
+//     const data = fragments.export(group);
+//     download(new File([new Blob([data])], `${groupId}.frag`));
+
+//     const properties = group.getLocalProperties();
+//     if (properties) {
+//       download(new File([JSON.stringify(properties)], `${groupId}.json`));
+//     }
+//   });
+// }
+//------------------
 function disposeFragments() {
   fragments.dispose();
     // Сброс input после удаления фрагмента
@@ -203,24 +212,17 @@ function disposeFragments() {
   
 }
 
-const stats = new Stats();
-stats.showPanel(2);
-document.body.append(stats.dom);
-stats.dom.style.left = "0px";
-stats.dom.style.zIndex = "unset";
-world.renderer.onBeforeUpdate.add(() => stats.begin());
-world.renderer.onAfterUpdate.add(() => stats.end());
-
 BUI.Manager.init();
-
+// <bim-panel active label="IFC Loader " >
+//</bim-panel>
 const panel = BUI.Component.create<BUI.PanelSection>(() => {
   return BUI.html`
-  <bim-panel active label="IFC Loader Tutorial" class="options-menu">
+ 
     <bim-panel-section collapsed label="Controls">
-      <bim-panel-section style="padding-top: 12px;">
+      <bim-panel-section style="padding-top: 2px;">
       
         <bim-button label="Load IFC"
-  @click="${() => {
+  @click="${() => {// loadOtherFile()
     const inputElement = document.getElementById('ifcInput');
     if (inputElement) {
       inputElement.click(); // Открываем диалог выбора файла только если элемент существует
@@ -233,6 +235,7 @@ const panel = BUI.Component.create<BUI.PanelSection>(() => {
         <bim-button label="Export fragments"
           @click="${() => {
             exportFragments();
+            // exportAllFragments()
           }}">
         </bim-button>  
             
@@ -244,24 +247,131 @@ const panel = BUI.Component.create<BUI.PanelSection>(() => {
       
       </bim-panel-section>
       
-    </bim-panel>
+    
   `;
 });
 
 document.body.append(panel);
+//-----------
+// const inputFrag = document.getElementById('fragInput');
+// if (inputFrag) {
+//   inputFrag.addEventListener('change', async (event) => {
+//     const inputElement = event.target as HTMLInputElement;
+//     inputFrag.addEventListener('change', async (event) => {
+//   const inputElement = event.target as HTMLInputElement;
+  
+//   if (inputElement.files && inputElement.files.length > 0) {
+//     let fragFile, jsonFile;
+//     for (const file of inputElement.files) {
+//       if (file.name.endsWith('.frag')) {
+//         fragFile = file;
+//       } else if (file.name.endsWith('.json')) {
+//         jsonFile = file;
+//       }
+//     }
+    
+//     if (fragFile && jsonFile) {
+//       // Загрузите и обработайте файлы .frag и .json
+//       await loadFragments(fragFile, jsonFile);
+//     } else {
+//       console.error("Both .frag and .json files are required");
+//     }
+//   }
+// });
+//   })
+// }
+// async function loadFrag(fragFile: { arrayBuffer: () => any; }, jsonFile: Blob) {
+//   // Загрузите содержимое fragFile и jsonFile
+//   // Пример загрузки содержимого файла .frag
+//   const fragData = await fragFile.arrayBuffer();
+//   const fragBuffer = new Uint8Array(fragData);
+  
+//   // Пример загрузки содержимого файла .json
+//   const jsonData = await new Promise((resolve) => {
+//     const reader = new FileReader();
+//     reader.onload = (e) => resolve(JSON.parse(e.target.result));
+//     reader.readAsText(jsonFile);
+//   });
+  
+//   // Теперь у вас есть fragBuffer и jsonData, которые можно использовать для загрузки фрагментов
+//   // Продолжите обработку данных здесь...
+// }
 
-const button = BUI.Component.create<BUI.PanelSection>(() => {
-  return BUI.html`
-      <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
-        @click="${() => {
-          if (panel.classList.contains("options-menu-visible")) {
-            panel.classList.remove("options-menu-visible");
-          } else {
-            panel.classList.add("options-menu-visible");
-          }
-        }}">
-      </bim-button>
-    `;
-});
 
-document.body.append(button);
+
+//  const loadFragmentsButton = document.getElementById('loadFragmentsButton') as HTMLButtonElement | null;
+//  if (loadFragmentsButton) {
+//   loadFragmentsButton.addEventListener('click', () => {
+//     inputFrag.click();
+//   });
+// }
+
+
+
+// let uuid = "";
+
+// async function loadFragments(fragFile?: File, jsonFile?: File) {
+//   if (fragments.groups.size) {
+//     return;
+//   }
+//   const fragFile = await fetch(
+//     "https://thatopen.github.io/engine_components/resources/small.frag",
+//   );
+//   const fragData = await fragFile.arrayBuffer();
+//   const fragBuffer = new Uint8Array(fragData);
+
+//   const jsonFile = await fetch(
+//     "https://thatopen.github.io/engine_components/resources/small.json",
+//   );
+//   const jsonData = await jsonFile.json();
+
+//   const group = await fragments.load(fragBuffer);
+//   group.setLocalProperties(jsonData); // Установите свойства, если метод доступен
+
+//   world.scene.three.add(group);
+//   uuid = group.uuid;
+// }
+// Получаем элемент input для загрузки .frag файлов
+const inputFrag = document.getElementById('fragInput');
+
+// Обработчик для кнопки, который вызывает клик по скрытому input
+const loadFragmentsButton = document.getElementById('loadFragmentsButton');
+if (loadFragmentsButton && inputFrag) {
+  loadFragmentsButton.addEventListener('click', () => {
+    inputFrag.click(); // Программно вызываем клик по скрытому input
+  });
+}
+
+// Обработчик события изменения для input, который загружает выбранный .frag файл
+if (inputFrag) {
+  inputFrag.addEventListener('change', async (event) => {
+    const inputElement = event.target as HTMLInputElement;
+    
+    if (inputElement.files && inputElement.files.length > 0) {
+      const fragFile = inputElement.files[0];
+      if (fragFile.name.endsWith('.frag')) {
+        await loadFrag(fragFile);
+      } else {
+        console.error("A .frag file is required");
+      }
+    }
+  });
+}
+
+// Функция для загрузки .frag файла и добавления его в сцену
+async function loadFrag(fragFile: File) {
+  const fragData = await fragFile.arrayBuffer();
+  const fragBuffer = new Uint8Array(fragData);
+  
+  // Проверяем, что fragmentIfcLoader и world.scene.three существуют
+  if (fragmentIfcLoader && world.scene.three) {
+    // Загружаем .frag-модель из Uint8Array
+    const fragmentsGroup = await fragmentIfcLoader.load(fragBuffer);
+    
+    // Добавляем загруженную модель в сцену
+    world.scene.three.add(fragmentsGroup);
+  }
+}
+
+ // Вызов функции loadIfc при загрузке страницы
+ document.addEventListener('DOMContentLoaded', loadIfc);
