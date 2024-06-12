3373d75b07b1e500bf7d57df406d00ca640a5502
Carduus0 <s.carduus@gmail.com>
2024-06-12 14:12:37 +0300

    add style to html

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import terser from "@rollup/plugin-terser";
+import serve from "rollup-plugin-serve";
 
 export default {
   input: "app.ts",
@@ -16,5 +17,12 @@ export default {
     commonjs(),
     typescript(), // Добавляем поддержку TypeScript
     // terser(), // Опционально: минификация для продакшн-сборки
+    serve({
+      // Настраиваем плагин serve
+      open: true, // Опционально: открыть в браузере автоматически
+      contentBase: ["", "public"], // Корневые директории для сервера
+      host: "localhost",
+      port: 3000, // Порт, на котором будет запущен сервер
+    }),
   ],
 };
